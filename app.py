cd0e46b288723cbc4dde41063cba2c175627e3b9
mrflib <35558715+mrflib@users.noreply.github.com>
2025-01-16 17:43:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ MAX_DAYS = 30         # how many days to allow
 STEP_MINUTES = 1      # how many minutes between each step (default 1)
 USE_CITY_SEARCH = True # toggle city input on or off
 DEBUG = True           # set to False if you want to hide debug prints
-SHOW_BULLETS = True    # new toggle to show/hide the bullet points
+SHOW_BULLETS = False    # new toggle to show/hide the bullet points
 ############################
 # END CONFIGURATION BLOCK
 ############################
